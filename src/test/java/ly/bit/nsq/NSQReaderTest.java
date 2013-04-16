2518cf3f4338fcda697a0d7a8ae53cbea49025b6
Andy O'Neill <oneill@energyhub.net>
2013-04-16 11:36:53 -0400

    Remove import * in tests to comply with bitly standards.

--- src/test/java/ly/bit/nsq/NSQReaderTest.java
+++ src/test/java/ly/bit/nsq/NSQReaderTest.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
-import java.util.*;
 
 /**
  * User: oneill
